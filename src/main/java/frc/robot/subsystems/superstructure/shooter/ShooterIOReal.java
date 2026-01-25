23749ffbecbda7f0e23e21502a48455bfc7851ae
Ayush Patwari <108635938+ayushpatwari@users.noreply.github.com>
2026-01-25 12:58:38 -0500

    Two motors shooter (#8)
    * changed to master and follower motor in shooter
    
    * fixed follower
    
    ---------
    
    Co-authored-by: vedhatamilinian <tamilinianvedha@gmail.com>

--- src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOReal.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VoltageOut;
@@ -11,6 +12,7 @@ import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.*;
 import frc.robot.constants.HardwareConstants;
@@ -19,106 +21,144 @@ import frc.robot.utils.ctre.RefreshAll;
 public class ShooterIOReal implements ShooterIO {
     final HardwareConstants.ShooterConstants constants;
 
-    private final TalonFX flywheelMotor;
+    private final TalonFX masterMotor;
+    private final TalonFX followerMotor;
 
-    private final StatusSignal<Angle> flywheelPosition;
-    private final StatusSignal<AngularVelocity> flywheelVelocity;
-    private final StatusSignal<Voltage> flywheelVoltage;
-    private final StatusSignal<Current> flywheelTorqueCurrent;
-    private final StatusSignal<Temperature> flywheelDeviceTemp;
+    private final StatusSignal<Angle> masterPosition;
+    private final StatusSignal<AngularVelocity> masterVelocity;
+    private final StatusSignal<Voltage> masterVoltage;
+    private final StatusSignal<Current> masterTorqueCurrent;
+    private final StatusSignal<Temperature> masterDeviceTemp;
+
+    private final StatusSignal<Angle> followerPosition;
+    private final StatusSignal<AngularVelocity> followerVelocity;
+    private final StatusSignal<Voltage> followerVoltage;
+    private final StatusSignal<Current> followerTorqueCurrent;
+    private final StatusSignal<Temperature> followerDeviceTemp;
 
     private final VelocityTorqueCurrentFOC velocityTorqueCurrentFOC;
     private final VoltageOut voltageOut;
     private final TorqueCurrentFOC torqueCurrentFOC;
+    private final Follower follower;
 
 
 
     public ShooterIOReal(final HardwareConstants.ShooterConstants constants) {
         this.constants = constants;
 
-        this.flywheelMotor = new TalonFX(constants.motorID(), constants.CANBus().toPhoenix6CANBus());
+        this.masterMotor = new TalonFX(constants.masterMotorID(), constants.CANBus().toPhoenix6CANBus());
+        this.followerMotor = new TalonFX(constants.followerMotorID(), constants.CANBus().toPhoenix6CANBus());
 
+        this.masterPosition = masterMotor.getPosition(false);
+        this.masterVelocity = masterMotor.getVelocity(false);
+        this.masterVoltage = masterMotor.getMotorVoltage(false);
+        this.masterTorqueCurrent = masterMotor.getTorqueCurrent(false);
+        this.masterDeviceTemp = masterMotor.getDeviceTemp(false);
 
-        this.flywheelPosition = flywheelMotor.getPosition(false);
-        this.flywheelVelocity = flywheelMotor.getVelocity(false);
-        this.flywheelVoltage = flywheelMotor.getMotorVoltage(false);
-        this.flywheelTorqueCurrent = flywheelMotor.getTorqueCurrent(false);
-        this.flywheelDeviceTemp = flywheelMotor.getDeviceTemp(false);
+        this.followerPosition = followerMotor.getPosition(false);
+        this.followerVelocity = followerMotor.getVelocity(false);
+        this.followerVoltage = followerMotor.getMotorVoltage(false);
+        this.followerTorqueCurrent = followerMotor.getTorqueCurrent(false);
+        this.followerDeviceTemp = followerMotor.getDeviceTemp(false);
 
         this.velocityTorqueCurrentFOC = new VelocityTorqueCurrentFOC(0);
         this.voltageOut = new VoltageOut(0);
         this.torqueCurrentFOC = new TorqueCurrentFOC(0);
+        this.follower = new Follower(masterMotor.getDeviceID(), MotorAlignmentValue.Opposed);
 
         RefreshAll.add(
                 constants.CANBus(),
-                flywheelPosition,
-                flywheelVelocity,
-                flywheelVoltage,
-                flywheelTorqueCurrent,
-                flywheelDeviceTemp
+                masterPosition,
+                masterVelocity,
+                masterVoltage,
+                masterTorqueCurrent,
+                masterDeviceTemp,
+                followerPosition,
+                followerVelocity,
+                followerVoltage,
+                followerTorqueCurrent,
+                followerDeviceTemp
         );
     }
 
     @Override
     public void config() {
-        final TalonFXConfiguration flywheelConfiguration = new TalonFXConfiguration();
-        flywheelConfiguration.Slot0 = new Slot0Configs()
+        final TalonFXConfiguration motorConfiguration = new TalonFXConfiguration();
+        motorConfiguration.Slot0 = new Slot0Configs()
                 .withKS(4.0212)
                 .withKV(0.40767)
                 .withKA(0.22711)
                 .withKP(30);
-        flywheelConfiguration.CurrentLimits.StatorCurrentLimit = 40;
-        flywheelConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLimit = 40;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLowerLimit = 40;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLowerTime = 1;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
-        flywheelConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        flywheelConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        flywheelConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
-        flywheelConfiguration.Feedback.SensorToMechanismRatio = constants.gearing();
-        flywheelMotor.getConfigurator().apply(flywheelConfiguration);
+        motorConfiguration.CurrentLimits.StatorCurrentLimit = 40;
+        motorConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfiguration.CurrentLimits.SupplyCurrentLimit = 40;
+        motorConfiguration.CurrentLimits.SupplyCurrentLowerLimit = 40;
+        motorConfiguration.CurrentLimits.SupplyCurrentLowerTime = 1;
+        motorConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+        motorConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+        motorConfiguration.Feedback.SensorToMechanismRatio = constants.gearing();
+
+        masterMotor.getConfigurator().apply(motorConfiguration);
+        motorConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        followerMotor.getConfigurator().apply(motorConfiguration);
 
         BaseStatusSignal.setUpdateFrequencyForAll(
                 100,
-                flywheelPosition,
-                flywheelVelocity,
-                flywheelVoltage,
-                flywheelTorqueCurrent
+                masterPosition,
+                masterVelocity,
+                masterVoltage,
+                masterTorqueCurrent,
+                followerPosition,
+                followerVelocity,
+                followerVoltage,
+                followerTorqueCurrent
         );
 
         BaseStatusSignal.setUpdateFrequencyForAll(
                 4,
-                flywheelDeviceTemp
+                masterDeviceTemp,
+                followerDeviceTemp
         );
 
         ParentDevice.optimizeBusUtilizationForAll(
                 4,
-                flywheelMotor
+                masterMotor,
+                followerMotor
         );
     }
 
     @Override
     public void updateInputs(ShooterIOInputs inputs) {
-        inputs.flywheelPositionRots = flywheelPosition.getValueAsDouble();
-        inputs.flywheelVelocityRotsPerSec = flywheelVelocity.getValueAsDouble();
-        inputs.flywheelVoltage = flywheelVoltage.getValueAsDouble();
-        inputs.flywheelTorqueCurrentAmps = flywheelTorqueCurrent.getValueAsDouble();
-        inputs.flywheelTemp = flywheelDeviceTemp.getValueAsDouble();
+        inputs.masterPositionRots = masterPosition.getValueAsDouble();
+        inputs.masterVelocityRotsPerSec = masterVelocity.getValueAsDouble();
+        inputs.masterVoltage = masterVoltage.getValueAsDouble();
+        inputs.masterTorqueCurrentAmps = masterTorqueCurrent.getValueAsDouble();
+        inputs.masterTempCelsius = masterDeviceTemp.getValueAsDouble();
+
+        inputs.followerPositionRots = followerPosition.getValueAsDouble();
+        inputs.followerVelocityRotsPerSec = followerVelocity.getValueAsDouble();
+        inputs.followerVoltage = followerVoltage.getValueAsDouble();
+        inputs.followerTorqueCurrentAmps = followerTorqueCurrent.getValueAsDouble();
+        inputs.followerTempCelsius = followerDeviceTemp.getValueAsDouble();
     }
 
     @Override
-    public void toFlywheelVelocity(final double velocityRotsPerSec) {
-        flywheelMotor.setControl(velocityTorqueCurrentFOC.withVelocity(velocityRotsPerSec));
+    public void toVelocity(final double velocityRotsPerSec) {
+        masterMotor.setControl(velocityTorqueCurrentFOC.withVelocity(velocityRotsPerSec));
+        followerMotor.setControl(follower);
     }
 
     @Override
-    public void toFlywheelVoltage(final double volts) {
-        flywheelMotor.setControl(voltageOut.withOutput(volts));
+    public void toVoltage(final double volts) {
+        masterMotor.setControl(voltageOut.withOutput(volts));
+        followerMotor.setControl(follower);
     }
 
     @Override
-    public void toFlywheelTorqueCurrent(final double torqueCurrentAmps) {
-        flywheelMotor.setControl(torqueCurrentFOC.withOutput(torqueCurrentAmps));
+    public void toTorqueCurrent(final double torqueCurrentAmps) {
+        masterMotor.setControl(torqueCurrentFOC.withOutput(torqueCurrentAmps));
+        followerMotor.setControl(follower);
     }
 }
--- src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterIOSim.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VoltageOut;
@@ -11,6 +12,7 @@ import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.sim.ChassisReference;
 import edu.wpi.first.math.system.plant.DCMotor;
@@ -25,139 +27,183 @@ import frc.robot.utils.control.DeltaTime;
 import frc.robot.utils.ctre.RefreshAll;
 import frc.robot.utils.sim.motors.TalonFXSim;
 
+import java.util.List;
+
 public class ShooterIOSim implements ShooterIO {
     private static final double SIM_UPDATE_PERIOD_SEC = 0.005;
 
     private final DeltaTime deltaTime;
     private final HardwareConstants.ShooterConstants constants;
 
-    private final TalonFX flywheelMotor;
-    private final TalonFXSim flywheelTalonFXSim;
+    private final TalonFX masterMotor;
+    private final TalonFX followerMotor;
 
-    private final StatusSignal<Angle> flywheelPosition;
-    private final StatusSignal<AngularVelocity> flywheelVelocity;
-    private final StatusSignal<Voltage> flywheelVoltage;
-    private final StatusSignal<Current> flywheelTorqueCurrent;
-    private final StatusSignal<Temperature> flywheelDeviceTemp;
+    private final TalonFXSim motorsSim;
+
+    private final StatusSignal<Angle> masterPosition;
+    private final StatusSignal<AngularVelocity> masterVelocity;
+    private final StatusSignal<Voltage> masterVoltage;
+    private final StatusSignal<Current> masterTorqueCurrent;
+    private final StatusSignal<Temperature> masterDeviceTemp;
+
+    private final StatusSignal<Angle> followerPosition;
+    private final StatusSignal<AngularVelocity> followerVelocity;
+    private final StatusSignal<Voltage> followerVoltage;
+    private final StatusSignal<Current> followerTorqueCurrent;
+    private final StatusSignal<Temperature> followerDeviceTemp;
 
     private final VelocityTorqueCurrentFOC velocityTorqueCurrentFOC;
     private final VoltageOut voltageOut;
     private final TorqueCurrentFOC torqueCurrentFOC;
+    private final Follower follower;
 
     public ShooterIOSim(final HardwareConstants.ShooterConstants constants) {
         this.deltaTime = new DeltaTime(true);
         this.constants = constants;
 
-        final DCMotorSim flywheelSim = new DCMotorSim(
+        final DCMotorSim motorsSim = new DCMotorSim(
                 LinearSystemId.createDCMotorSystem(
-                        DCMotor.getKrakenX60Foc(1),
+                        DCMotor.getKrakenX60Foc(2),
                         SimConstants.Shooter.MOMENT_OF_INERTIA,
                         constants.gearing()
                 ),
-                DCMotor.getKrakenX60Foc(1)
+                DCMotor.getKrakenX60Foc(2)
         );
 
-        this.flywheelMotor = new TalonFX(constants.motorID(), constants.CANBus().toPhoenix6CANBus());
+        this.masterMotor = new TalonFX(constants.masterMotorID(), constants.CANBus().toPhoenix6CANBus());
+        this.followerMotor = new TalonFX(constants.followerMotorID(), constants.CANBus().toPhoenix6CANBus());
 
-        this.flywheelTalonFXSim = new TalonFXSim(
-                flywheelMotor,
+        this.motorsSim = new TalonFXSim(
+                List.of(masterMotor, followerMotor),
                 constants.gearing(),
-                flywheelSim::update,
-                flywheelSim::setInputVoltage,
-                flywheelSim::getAngularPositionRad,
-                flywheelSim::getAngularVelocityRadPerSec
+                motorsSim::update,
+                motorsSim::setInputVoltage,
+                motorsSim::getAngularPositionRad,
+                motorsSim::getAngularVelocityRadPerSec
         );
 
-        this.flywheelPosition = flywheelMotor.getPosition(false);
-        this.flywheelVelocity = flywheelMotor.getVelocity(false);
-        this.flywheelVoltage = flywheelMotor.getMotorVoltage(false);
-        this.flywheelTorqueCurrent = flywheelMotor.getTorqueCurrent(false);
-        this.flywheelDeviceTemp = flywheelMotor.getDeviceTemp(false);
+        this.masterPosition = masterMotor.getPosition(false);
+        this.masterVelocity = masterMotor.getVelocity(false);
+        this.masterVoltage = masterMotor.getMotorVoltage(false);
+        this.masterTorqueCurrent = masterMotor.getTorqueCurrent(false);
+        this.masterDeviceTemp = masterMotor.getDeviceTemp(false);
+
+        this.followerPosition = followerMotor.getPosition(false);
+        this.followerVelocity = followerMotor.getVelocity(false);
+        this.followerVoltage = followerMotor.getMotorVoltage(false);
+        this.followerTorqueCurrent = followerMotor.getTorqueCurrent(false);
+        this.followerDeviceTemp = followerMotor.getDeviceTemp(false);
 
         this.velocityTorqueCurrentFOC = new VelocityTorqueCurrentFOC(0);
         this.voltageOut = new VoltageOut(0);
         this.torqueCurrentFOC = new TorqueCurrentFOC(0);
+        this.follower = new Follower(masterMotor.getDeviceID(), MotorAlignmentValue.Aligned);
 
         RefreshAll.add(
                 constants.CANBus(),
-                flywheelPosition,
-                flywheelVelocity,
-                flywheelVoltage,
-                flywheelTorqueCurrent,
-                flywheelDeviceTemp
+                masterPosition,
+                masterVelocity,
+                masterVoltage,
+                masterTorqueCurrent,
+                masterDeviceTemp,
+                followerPosition,
+                followerVelocity,
+                followerVoltage,
+                followerTorqueCurrent,
+                followerDeviceTemp
         );
 
         final Notifier simUpdateNotifier = new Notifier(() -> {
             final double dt = deltaTime.get();
-            flywheelTalonFXSim.update(dt);
+            this.motorsSim.update(dt);
         });
         ToClose.add(simUpdateNotifier);
         simUpdateNotifier.setName(String.format(
-                "SimUpdate(%d)",
-                flywheelMotor.getDeviceID()
+                "SimUpdate(%d,%d)",
+                masterMotor.getDeviceID(),
+                followerMotor.getDeviceID()
         ));
         simUpdateNotifier.startPeriodic(SIM_UPDATE_PERIOD_SEC);
     }
 
     @Override
     public void config() {
-        final TalonFXConfiguration flywheelConfiguration = new TalonFXConfiguration();
-        flywheelConfiguration.Slot0 = new Slot0Configs()
+        final TalonFXConfiguration motorConfiguration = new TalonFXConfiguration();
+        motorConfiguration.Slot0 = new Slot0Configs()
                 .withKS(0.01)
                 .withKP(200);
-        flywheelConfiguration.CurrentLimits.StatorCurrentLimit = 70;
-        flywheelConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLimit = 60;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLowerLimit = 40;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLowerTime = 1;
-        flywheelConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
-        flywheelConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        flywheelConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        flywheelConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
-        flywheelConfiguration.Feedback.SensorToMechanismRatio = constants.gearing();
-        flywheelMotor.getConfigurator().apply(flywheelConfiguration);
+        motorConfiguration.CurrentLimits.StatorCurrentLimit = 70;
+        motorConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfiguration.CurrentLimits.SupplyCurrentLimit = 60;
+        motorConfiguration.CurrentLimits.SupplyCurrentLowerLimit = 40;
+        motorConfiguration.CurrentLimits.SupplyCurrentLowerTime = 1;
+        motorConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+        motorConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+        motorConfiguration.Feedback.SensorToMechanismRatio = constants.gearing();
+
+        masterMotor.getConfigurator().apply(motorConfiguration);
+        motorConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        followerMotor.getConfigurator().apply(motorConfiguration);
 
         BaseStatusSignal.setUpdateFrequencyForAll(
                 100,
-                flywheelPosition,
-                flywheelVelocity,
-                flywheelVoltage,
-                flywheelTorqueCurrent
+                masterPosition,
+                masterVelocity,
+                masterVoltage,
+                masterTorqueCurrent,
+                followerPosition,
+                followerVelocity,
+                followerVoltage,
+                followerTorqueCurrent
         );
 
         BaseStatusSignal.setUpdateFrequencyForAll(
                 4,
-                flywheelDeviceTemp
+                masterDeviceTemp,
+                followerDeviceTemp
         );
 
         ParentDevice.optimizeBusUtilizationForAll(
                 4,
-                flywheelMotor
+                masterMotor,
+                followerMotor
         );
 
-        flywheelMotor.getSimState().Orientation = ChassisReference.Clockwise_Positive;
+        masterMotor.getSimState().Orientation = ChassisReference.Clockwise_Positive;
+        followerMotor.getSimState().Orientation = ChassisReference.Clockwise_Positive;
     }
 
     @Override
     public void updateInputs(ShooterIOInputs inputs) {
-        inputs.flywheelPositionRots = flywheelPosition.getValueAsDouble();
-        inputs.flywheelVelocityRotsPerSec = flywheelVelocity.getValueAsDouble();
-        inputs.flywheelVoltage = flywheelVoltage.getValueAsDouble();
-        inputs.flywheelTorqueCurrentAmps = flywheelTorqueCurrent.getValueAsDouble();
-        inputs.flywheelTemp = flywheelDeviceTemp.getValueAsDouble();
+        inputs.masterPositionRots = masterPosition.getValueAsDouble();
+        inputs.masterVelocityRotsPerSec = masterVelocity.getValueAsDouble();
+        inputs.masterVoltage = masterVoltage.getValueAsDouble();
+        inputs.masterTorqueCurrentAmps = masterTorqueCurrent.getValueAsDouble();
+        inputs.masterTempCelsius = masterDeviceTemp.getValueAsDouble();
+
+        inputs.followerPositionRots = followerPosition.getValueAsDouble();
+        inputs.followerVelocityRotsPerSec = followerVelocity.getValueAsDouble();
+        inputs.followerVoltage = followerVoltage.getValueAsDouble();
+        inputs.followerTorqueCurrentAmps = followerTorqueCurrent.getValueAsDouble();
+        inputs.followerTempCelsius = followerDeviceTemp.getValueAsDouble();
     }
     @Override
-    public void toFlywheelVelocity(final double velocityRotsPerSec) {
-        flywheelMotor.setControl(velocityTorqueCurrentFOC.withVelocity(velocityRotsPerSec));
+    public void toVelocity(final double velocityRotsPerSec) {
+        masterMotor.setControl(velocityTorqueCurrentFOC.withVelocity(velocityRotsPerSec));
+        followerMotor.setControl(follower);
     }
 
     @Override
-    public void toFlywheelVoltage(final double volts) {
-        flywheelMotor.setControl(voltageOut.withOutput(volts));
+    public void toVoltage(final double volts) {
+        masterMotor.setControl(voltageOut.withOutput(volts));
+        followerMotor.setControl(follower);
     }
 
     @Override
-    public void toFlywheelTorqueCurrent(final double torqueCurrentAmps) {
-        flywheelMotor.setControl(torqueCurrentFOC.withOutput(torqueCurrentAmps));
+    public void toTorqueCurrent(final double torqueCurrentAmps) {
+        masterMotor.setControl(torqueCurrentFOC.withOutput(torqueCurrentAmps));
+        followerMotor.setControl(follower);
     }
 }
