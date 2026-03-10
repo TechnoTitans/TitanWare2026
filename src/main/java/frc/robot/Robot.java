4b4e3711c1b906db8c1b1817c109ee44b4b90258
Max <maxgoog06@outlook.com>
2026-03-10 12:03:49 -0400

    moved AllianceShift out of robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -158,7 +158,7 @@ public class Robot extends LoggedRobot {
             .and(teleopEnabled);
 
     private final LoggedTrigger hubActive =
-            group.t("HubActive", () -> AllianceShift.get().hubStatus() == HubStatus.ACTIVE);
+            group.t("HubActive", () -> AllianceShift.get().hubStatus() == AllianceShift.HubStatus.ACTIVE);
 
     public Robot() {
         if ((RobotBase.isReal() && Constants.CURRENT_MODE != Constants.RobotMode.REAL) ||
@@ -387,96 +387,5 @@ public class Robot extends LoggedRobot {
         coController.rightTrigger(0.5, teleopEventLoop)
                 .whileTrue(shootCommands.stopAndShoot())
                 .onFalse(intake.deploy());
-    }
-
-    public enum HubStatus {
-        ACTIVE,
-        INACTIVE,
-        UNKNOWN
-    }
-
-    public enum AllianceShift {
-        AUTO,
-        TRANSITION,
-        SHIFT_1,
-        SHIFT_2,
-        SHIFT_3,
-        SHIFT_4,
-        ENDGAME,
-        UNKNOWN;
-
-        public HubStatus hubStatus() {
-            if (this == AUTO || this == TRANSITION || this == ENDGAME) {
-                return HubStatus.ACTIVE;
-            } else if (this == UNKNOWN) {
-                return HubStatus.UNKNOWN;
-            }
-
-            final String gameMessage = DriverStation.getGameSpecificMessage();
-            if (gameMessage.isEmpty()) {
-                return HubStatus.UNKNOWN;
-            }
-
-            final Optional<DriverStation.Alliance> maybeAlliance = DriverStation.getAlliance();
-            if (maybeAlliance.isEmpty()) {
-                return HubStatus.UNKNOWN;
-            }
-
-            final DriverStation.Alliance alliance = maybeAlliance.get();
-            final DriverStation.Alliance autoWinningAlliance;
-            switch (gameMessage.charAt(0)) {
-                case 'R' -> autoWinningAlliance = DriverStation.Alliance.Red;
-                case 'B' -> autoWinningAlliance = DriverStation.Alliance.Blue;
-                default -> {
-                    return HubStatus.UNKNOWN;
-                }
-            }
-
-            final boolean wonAuto = alliance == autoWinningAlliance;
-            return switch (this) {
-                case SHIFT_1, SHIFT_3 -> wonAuto ? HubStatus.INACTIVE : HubStatus.ACTIVE;
-                case SHIFT_2, SHIFT_4 -> wonAuto ? HubStatus.ACTIVE : HubStatus.INACTIVE;
-                default -> HubStatus.UNKNOWN;
-            };
-        }
-
-        public static AllianceShift get() {
-            if (DriverStation.isAutonomousEnabled()) {
-                return AUTO;
-            } else if (DriverStation.isDisabled()) {
-                return UNKNOWN;
-            }
-
-            final double matchTime = DriverStation.getMatchTime();
-            if (DriverStation.isFMSAttached()) {
-                if (matchTime > 130) {
-                    return TRANSITION;
-                } else if (matchTime > 105) {
-                    return SHIFT_1;
-                } else if (matchTime > 80) {
-                    return SHIFT_2;
-                } else if (matchTime > 55) {
-                    return SHIFT_3;
-                } else if (matchTime > 30) {
-                    return SHIFT_4;
-                } else {
-                    return ENDGAME;
-                }
-            } else {
-                if (matchTime > 110) {
-                    return ENDGAME;
-                } else if (matchTime > 85) {
-                    return SHIFT_4;
-                } else if (matchTime > 60) {
-                    return SHIFT_3;
-                } else if (matchTime > 35) {
-                    return SHIFT_2;
-                } else if (matchTime > 10) {
-                    return SHIFT_1;
-                } else {
-                    return TRANSITION;
-                }
-            }
-        }
     }
 }
